188cb42110eb5b584dd154d0689aa9570f1d6a16
MSHTALI <fake.overlord@gmail.com>
2018-05-16 23:02:55 +0430

    Update Shitty QR rev 0.1.py

--- Shitty QR rev 0.1.py
+++ Shitty QR rev 0.1.py
@@ -1,34 +1,32 @@
+# playing around with turtle
+import turtle 
+print ("find out what does this website looks like in shitty QR")
 text = input("enter URL : ")
 text = hash(text)
-b = str(text)
+pirate = str(text)
 ninja = []
-
-colors = ['red', 'black', 'gray','yellow', 'green', 'navy', 'white','red', 'black', 'white','yellow', 'green', 'navy','red', 'black', 'white','yellow', 'green', 'navy']
-
-for digit in b:
+colors = ['red', 'blue', 'green','yellow', 'white', 'navy',
+          'purple','orange', 'brown', 'pink']
+for digit in pirate:
     ninja.append (str(digit))
 ninja.pop(0)
-print (ninja)
-
-
-import turtle 
-
+print ("( ͡° ͜ʖ ͡°)")
 shelly = turtle.Turtle()
-
+turtle.bgcolor('black')
 shelly.speed(10)
 for i in range(19):
-    shelly.pencolor(colors[i%12])
+    
+    shelly.width(i/10 + 5)
+    shelly.pencolor(colors[i%10])
     shelly.forward(100)
     shelly.right(90)
     shelly.forward(10 * int(ninja[i]))
     shelly.left(90)
     shelly.forward(10 * int(ninja[i]))
     shelly.right(60)
-    
     shelly.penup()
     shelly.setposition(0, 0)
     shelly.pendown()
-    
     shelly.right(30)
     
 turtle.done()
